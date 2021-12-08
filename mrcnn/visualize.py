8e268180c9839db154f23b06146677b9083fd6c2
verneh <verneh@gmail.com>
2021-12-08 12:20:30 +0100

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -122,7 +122,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
 
     masked_image = image.astype(np.uint32).copy()
     for i in range(N):
-        color = colors[i]
+        # color = colors[i]
+        color = 'black'
 
         # Bounding box
         if not np.any(boxes[i]):
